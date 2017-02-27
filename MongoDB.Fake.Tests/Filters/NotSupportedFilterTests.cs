2abfae61009e8760fdf48da09f9192c4321609bf
Nikolay Balakin <n@balakin.me>
2017-02-27 18:43:50 +0200

    Add test for unknown operator

---
+++ MongoDB.Fake.Tests/Filters/NotSupportedFilterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Xunit;
+
+namespace MongoDB.Fake.Tests.Filters
+{
+    public class NotSupportedFilterTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var collection = new FakeMongoCollection<SimpleTestDocument>();
+
+            var bson = BsonDocument.Parse("{IntField:{$unsupportedOperator:1}}");
+            var filter = new BsonDocumentFilterDefinition<SimpleTestDocument>(bson);
+
+            Action action = () => collection.Find(filter).ToList();
+            action.ShouldThrow<NotSupportedException>();
+        }
+    }
+}
