2ae7f6fbe8fd6661b7ca3d11a5bf0cfe84344a6e
Nikolay Balakin <n@balakin.me>
2017-02-28 18:45:01 +0200

    Add one more test for $eq operator

---
+++ MongoDB.Fake.Tests/Filters/Cases/Empty/BuilderWhereTrue.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDB.Fake.Tests.Filters.Cases.Empty
+{
+    internal class BuilderWhereTrue : EmptyTestCaseBase
+    {
+        public override FilterDefinition<SimpleTestDocument> GetFilter()
+        {
+            var builder = Builders<SimpleTestDocument>.Filter;
+            return builder.Where(d => true);
+        }
+    }
+}
