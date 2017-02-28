0910343d91afbef736d138a8b0be66d76e18d610
Nikolay Balakin <n@balakin.me>
2017-02-28 18:48:54 +0200

    Implement $or operator
     #9

---
+++ MongoDB.Fake/Filters/OrFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
+
+namespace MongoDB.Fake.Filters
+{
+    internal class OrFilter : IFilter
+    {
+        private readonly ICollection<IFilter> _children;
+
+        public OrFilter(ICollection<IFilter> children)
+        {
+            _children = children;
+        }
+
+        public Boolean Filter(BsonValue value)
+        {
+            return _children.Any(filter => filter.Filter(value));
+        }
+    }
+}
