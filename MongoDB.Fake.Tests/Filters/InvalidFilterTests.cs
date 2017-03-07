2d9b9f9241a2055918effea0d573438360483e0c
Nikolay Balakin <n@balakin.me>
2017-03-07 20:31:25 +0200

    Add test for invalid $and operator

---
+++ MongoDB.Fake.Tests/Filters/InvalidFilterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Xunit;
+
+namespace MongoDB.Fake.Tests.Filters
+{
+    public class InvalidFilterTests
+    {
+        [Fact]
+        public void UnsupportedOperatorThrowsNotSupportedException()
+        {
+            Test<NotSupportedException>("{IntField:{$unsupportedOperator:1}}");
+        }
+
+        [Fact]
+        public void IncorrectAndFilterThrowsArgumentOutOfRangeException()
+        {
+            Test<ArgumentOutOfRangeException>("{$and:{}}");
+        }
+
+        private void Test<TException>(string json)
+            where TException : Exception
+        {
+            var collection = new FakeMongoCollection<SimpleTestDocument>();
+
+            var bson = BsonDocument.Parse(json);
+            var filter = new BsonDocumentFilterDefinition<SimpleTestDocument>(bson);
+
+            Action action = () => collection.Find(filter).ToList();
+            action.ShouldThrow<TException>();
+        }
+    }
+}
